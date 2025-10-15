44f803bbeeb8847b90eb4b31356051d1a2850552
shouvikofficial <shouvikdhaliofficial@gmail.com>
2025-10-15 21:10:04 +0600

    delete files

--- api/classes.php
+++
@@ -1,5 +0,0 @@
-<?php
-require '../includes/db.php';
-$rows = $pdo->query("SELECT id, title, start_datetime AS start, end_datetime AS end FROM classes")->fetchAll();
-header('Content-Type: application/json');
-echo json_encode($rows);
